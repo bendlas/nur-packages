a93b323f0b154d065fde3cad85e5eee143531e89
Herwig Hochleitner <herwig@bendlas.net>
2023-06-21 13:46:28 +0200

    lsd: 0.18.0 -> 0.23.1

--- pkgs/lsd/default.nix
+++ pkgs/lsd/default.nix
@@ -1,21 +1,21 @@
-{ stdenv, rustPlatform, fetchFromGitHub }:
+{ lib, rustPlatform, fetchFromGitHub }:
 
 rustPlatform.buildRustPackage rec {
   name = "lsd-${version}";
-  version = "0.18.0";
+  version = "0.23.1";
 
   src = fetchFromGitHub {
-    owner = "Peltoche";
+    owner = "lsd-rs";
     repo = "lsd";
     rev = "${version}";
-    sha256 = "006fy87jrb77cpa6bywchcvq1p74vlpy151q1j4nsj8npbr02krj";
+    sha256 = "sha256-FY1odcKBl7zJ+MxfohkmC1e45fPQK3MKB3orQdCRpA4=";
   };
 
-  cargoSha256 = "0mrvcca9y0vylcrbfxxba45v05qxd8z91vb4in88px60xah0dy3q";
+  cargoSha256 = "sha256-t7J7hIbLlRq99Yd2/3Zn+PbHhJtaJRdDluDXN0Hp/Jc=";
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "The next gen ls command";
-    homepage = https://github.com/Peltoche/lsd;
+    homepage = https://github.com/lsd-rs/lsd;
     license = licenses.asl20;
     maintainers = [ maintainers.bendlas ];
     platforms = platforms.all;
